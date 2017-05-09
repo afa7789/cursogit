494c48ba57523311337fc6e0352c5f941d07046d
Rodrigo Silva Borges <rodrigo.sil.borges@gmail.com>
2017-05-09 17:53:14 -0300

    ordena vetor

--- nome.c
+++ nome.c
@@ -1,5 +1,21 @@
-include <stdio.h>
+#include <stdio.h>
 
-main(){
+int main(){
 	printf("Meu Nome");
+
+	int v[] = [4 2 5 1 9 7 8 0 3 6];
+	int i, j;
+
+	for(i=0; i<10; i++) {
+		for(j=i; j<10; j++) {
+			if(v[j] > v[j+1]) {
+			
+				int a = v[j];
+				v[j] = v[j+1];
+				v[j+1] = v[j];
+			}
+		}
+	}
+
+	return 0;
 }
