d2acf2878235b419c9d74ffe6b7b035a8b6578a5
Jonathan <01jonathanh10@gmail.com>
2017-05-09 18:05:57 -0300

    Branch

--- luis.c
+++ luis.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
 void main(){
-	printf("Luis Felipe Arantes");
+	int n[10]={6,12,541,76432,12,3254,612,763,123,412};
+	
+	int i = 0;
+	int j = 0;
+	int menor = 0;
+	
+	for (i=0;i<10;i++)
+	{
+		menor = n[i];
+		for (j=i;j<10;j++)
+		{
+			
+		}
+	}
 
 }
